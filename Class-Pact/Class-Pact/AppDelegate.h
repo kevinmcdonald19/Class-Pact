351e37cbeb6f40bd8c1d495114e1659f51e355f8
Kevin <kevinmcdonald19@gmail.com>
2012-04-20 22:43:09 -0500

    project

---
+++ Class-Pact/Class-Pact/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Class-Pact
+//
+//  Created by Kevin Mcdonald on 4/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Class-Pact/Class-PactTests/Class_PactTests.h
@@ -0,0 +1,13 @@
+//
+//  Class_PactTests.h
+//  Class-PactTests
+//
+//  Created by Kevin Mcdonald on 4/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Class_PactTests : SenTestCase
+
+@end
