56c854f0f089da6c1402389af80fae039857f3b1
Kevin <kevinmcdonald19@gmail.com>
2012-04-20 21:16:58 -0500

    commiting first project

---
+++ Class-Pact/Class-Pact/Class-Pact/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Class-Pact
+//
+//  Created by Kevin Mcdonald on 4/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
